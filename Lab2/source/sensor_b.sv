16810b2ad90c997eb3805d86a603175704e4dada
Richard <zhangruichao2000@gmail.com>
2020-09-06 19:14:45 +0800

    finish sensor_b and sensor_d v1

---
+++ Lab2/source/sensor_b.sv
@@ -0,0 +1,10 @@
+module sensor_b (
+    input wire [3:0] sensors,
+    output reg error;
+);
+
+    always_comb begin 
+        error = sensors[1] & sensors[2] | sensors[1] & sensors[3] | sensors[0];
+    end
+    
+endmodule
--- Lab2/source/sensor_d.sv
+++ Lab2/source/sensor_d.sv
@@ -1,6 +1,6 @@
 module sensor_d (
-    input wire [3:0] sensors;
-    output wire error;
+    input wire [3:0] sensors,
+    output wire error
 );
 
     assign error = sensors[1] & sensors[2] | sensors[1] & sensors[3] | sensors[0];
