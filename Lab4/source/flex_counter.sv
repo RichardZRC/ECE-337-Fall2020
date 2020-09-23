dc74088007379a2d474407efb17285d3c7cc8683
Richard <zhangruichao2000@gmail.com>
2020-09-23 16:37:12 +0800

    debugging flex counter

--- Lab4/source/flex_counter.sv
+++ Lab4/source/flex_counter.sv
@@ -18,17 +18,10 @@ module flex_counter
     always_ff @ (posedge clk, negedge n_rst) begin
         if (n_rst == 0) begin
             count_out <= 0;
-        end
-        else begin
-            count_out <= next_count;
-        end
-    end
-    
-    always_ff @ (posedge clk, negedge n_rst) begin
-        if (n_rst == 0) begin
             rollover_flag <= 0;
         end
         else begin
+            count_out <= next_count;
             rollover_flag <= rollover_flag_temp;
         end
     end
