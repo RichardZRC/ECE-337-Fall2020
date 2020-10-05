3bc3b5216cfab8d6f2329cbeac91f27f22bc6279
Richard <zhangruichao2000@gmail.com>
2020-10-05 18:48:26 +0800

    debugging timer

--- Lab6/source/timer.sv
+++ Lab6/source/timer.sv
@@ -7,6 +7,7 @@ module timer (
 );
 
     reg [3:0] clk_count;
+    reg [3:0] bit_count;
     reg clk_rollover_flag;
     reg bit_rollover_flag;
     wire timer_clear;
@@ -17,7 +18,7 @@ module timer (
     assign shift_enable = (clk_count == 4'd5);
 
     flex_counter #(.NUM_CNT_BITS(4)) BIT_COUNTER (.clk(shift_enable), .n_rst(n_rst), .clear(timer_clear), .count_enable(enable_timer), 
-    .rollover_val(4'b1000), .count_out(clk_count), .rollover_flag(bit_rollover_flag));
+    .rollover_val(4'b1000), .count_out(bit_count), .rollover_flag(bit_rollover_flag));
 
     assign packet_done = bit_rollover_flag;
 
