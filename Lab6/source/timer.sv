0b0af9bb3b820e8dbc4624cf2492945daaab79eb
Richard <zhangruichao2000@gmail.com>
2020-10-05 21:54:12 +0800

    debuggng timer

--- Lab6/source/timer.sv
+++ Lab6/source/timer.sv
@@ -15,7 +15,7 @@ module timer (
     flex_counter #(.NUM_CNT_BITS(4)) CLK_COUNTER (.clk(clk), .n_rst(n_rst), .clear(timer_clear), .count_enable(enable_timer), 
     .rollover_val(4'b1010), .count_out(clk_count), .rollover_flag(clk_rollover_flag));
 
-    assign shift_enable = （(clk_count == 4'd5) && (bit_count != 4'b0000));
+    assign shift_enable = ((clk_count == 4'd5) && (bit_count != 4'b0000));
 
     flex_counter #(.NUM_CNT_BITS(4)) BIT_COUNTER (.clk(shift_enable), .n_rst(n_rst), .clear(timer_clear), .count_enable(enable_timer), 
     .rollover_val(4'b1001), .count_out(bit_count), .rollover_flag(bit_rollover_flag));
