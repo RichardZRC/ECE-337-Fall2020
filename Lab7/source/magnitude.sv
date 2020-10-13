cece2ad0083c1410802d91a9d01146efeacecc29
Richard <zhangruichao2000@gmail.com>
2020-10-13 17:56:03 +0800

    finish magnitude v1

---
+++ Lab7/source/magnitude.sv
@@ -0,0 +1,18 @@
+module magnitude (
+    input wire [16:0] in,
+    output wire [15:0] out
+);
+    reg [16:0] mag;
+    reg [16:0] mag_temp;
+
+    always_comb begin: 
+        mag = in;
+        if (mag[16] == 1'b1) begin
+            mag[15:0] = ~in[15:0];
+            mag[15:0] = mag[15:0] + 1;
+        end
+    end
+
+    assign out = mag[15:0];
+
+endmodule
