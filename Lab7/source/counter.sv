3395b07f4608fc3c56f04c97a029d74b6131bef1
Richard <zhangruichao2000@gmail.com>
2020-10-13 17:46:13 +0800

    finish counter v1

---
+++ Lab7/source/counter.sv
@@ -0,0 +1,13 @@
+module counter (
+    input wire clk,
+    input wire n_rst,
+    input wire cnt_up,
+    input wire clear,
+    output reg one_k_samples
+);
+
+    reg [9:0] count_out;
+    flex_counter #(.NUM_CNT_BITS(10)) counter (.clk(clk), .n_rst(n_rst), .clear(clear),
+    .count_enable(cnt_up), .rollover_value(10'd1000), .count_out(count_out), .rollover_flag(one_k_samples));
+
+endmodule
