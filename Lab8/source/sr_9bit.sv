7323699dc963a0912c36906da0f5479876e36112
Richard <zhangruichao2000@gmail.com>
2020-10-18 21:00:29 +0800

    modify uart receiver

--- Lab8/source/sr_9bit.sv
+++ Lab8/source/sr_9bit.sv
@@ -3,7 +3,8 @@ module sr_9bit (
     input wire n_rst,
     input wire shift_strobe,
     input wire serial_in,
-    output wire [7:0] packet_data,
+    input wire [3:0] data_size,
+    output reg [7:0] packet_data,
     output wire stop_bit
 );
 
@@ -11,7 +12,16 @@ module sr_9bit (
     flex_stp_sr #(.NUM_BITS(9), .SHIFT_MSB(0)) SR1 (.clk(clk), .n_rst(n_rst), .serial_in(serial_in),
      .shift_enable(shift_strobe), .parallel_out(parallel_out));
     
-    assign packet_data = parallel_out[7:0];
+    // assign packet_data = parallel_out[7:0];
     assign stop_bit = parallel_out[8];
 
+    always_comb begin: 
+        if (data_size == 5) begin
+            packet_data = {3'b0, parallel_out[7:3]};
+        end else if (data_size == 7) begin
+            packet_data = {1'b0, parallel_out[7:1]};
+        end else begin
+            packet_data = parallel_out[7:0];
+        end
+    end
 endmodule
--- Lab8/source/timer.sv
+++ Lab8/source/timer.sv
@@ -2,6 +2,8 @@ module timer (
     input wire clk,
     input wire n_rst,
     input wire enable_timer,
+    input wire [13:0] bit_period,
+    input wire [3:0] data_size,
     output wire shift_enable,
     output wire packet_done
     //TODO: delete test signal
@@ -14,13 +16,13 @@ module timer (
     reg bit_rollover_flag;
     wire timer_clear;
     assign timer_clear = (enable_timer == 1'b0);
-    flex_counter #(.NUM_CNT_BITS(4)) CLK_COUNTER (.clk(clk), .n_rst(n_rst), .clear(timer_clear), .count_enable(enable_timer), 
-    .rollover_val(4'b1010), .count_out(clk_count), .rollover_flag(clk_rollover_flag));
+    flex_counter #(.NUM_CNT_BITS(14)) CLK_COUNTER (.clk(clk), .n_rst(n_rst), .clear(timer_clear), .count_enable(enable_timer), 
+    .rollover_val(bit_period), .count_out(clk_count), .rollover_flag(clk_rollover_flag));
 
-    assign shift_enable = (clk_count == 4'd3);
+    assign shift_enable = (clk_count == (bit_period / 2 - 1));
 
     flex_counter #(.NUM_CNT_BITS(4)) BIT_COUNTER (.clk(clk), .n_rst(n_rst), .clear(timer_clear), .count_enable(shift_enable), 
-    .rollover_val(4'b1010), .count_out(bit_count), .rollover_flag(bit_rollover_flag));
+    .rollover_val(data_size), .count_out(bit_count), .rollover_flag(bit_rollover_flag));
 
     assign packet_done = bit_rollover_flag;
 
