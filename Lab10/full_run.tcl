e766d4fe23c29f8197a259f8b0b84f28fadbdd43
Richard <zhangruichao2000@gmail.com>
2020-11-03 17:00:49 +0800

    add metal fill

--- Lab10/full_run.tcl
+++ Lab10/full_run.tcl
@@ -76,7 +76,7 @@ addFiller -cell FILL
 setMetalFill -layer "1 2 3" -preferredDensity 35 -gapSpacing 0.2 \
              -minWidth 0.1 -maxWidth 1.0 \
              -minLength 0.1 -maxLength 10.0
-#addMetalFill -layer "1 2 3"
+addMetalFill -layer "1 2 3"
 
 
 # Connect all new cells to VDD/GND
