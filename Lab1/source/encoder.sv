1e85dac8f66017df9d029e522eff3399e2148187
Richard <zhangruichao2000@gmail.com>
2020-11-30 12:19:47 +0800

    debug

--- Lab1/source/encoder.sv
+++ Lab1/source/encoder.sv
@@ -1,9 +1,9 @@
-module 31t5 encoder (
+module encoder (
 	input wire [30:0] in,
 	output reg [4:0] out
 );
 	integer i;
-	always_comb begin: 
+	always_comb begin
 		out = '0;
 		for (i = 30; i >= 0; i = i - 1) begin
 			if (in[i] == 1) begin
@@ -11,5 +11,4 @@ module 31t5 encoder (
 			end
 		end
 	end
-endmodule
 endmodule
