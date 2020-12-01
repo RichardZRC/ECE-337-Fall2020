6ff5ef46fb405bf3e8511b4d417e81ea8daed578
Richard <zhangruichao2000@gmail.com>
2020-12-01 21:01:42 +0800

    debug

--- Lab12/source/timer.sv
+++ Lab12/source/timer.sv
@@ -9,13 +9,17 @@ module timer (
 );
     reg [3:0] clk_count1, clk_count2;
     reg [3:0] bit_count1, bit_count2;
+    reg state, next_state, cancel_next;
     reg clk_rollover_flag;
     reg bit_rollover_flag;
-    wire shift2;
+    wire shift2, shift3;
     wire timer_clear;
     wire timer_enable;
     
-    assign timer_enable = enable && (~stuff_bit);
+    assign timer_enable = enable;
+
+    localparam IDLE = 1'b0;
+    localparam CANCEL = 1'b1;
 
     flex_counter #(.NUM_CNT_BITS(4)) CLK_COUNTER (
         .clk(clk),
@@ -49,16 +53,41 @@ module timer (
         .rollover_flag()
     );
 
-    assign shift2 = (clk_count2 == 4'd1);
-
     flex_counter #(.NUM_CNT_BITS(4)) BIT_COUNTER_2 (
         .clk(clk),
         .n_rst(n_rst),
         .clear(clear),
-        .count_enable(shift2),
+        .count_enable(shift3),
         .rollover_val(4'b1000),
         .count_out(bit_count2),
         .rollover_flag(one_byte_time)
     );
+
+    assign shift2 = (clk_count2 == 4'd1);
+    assign shift3 = shift2 && (~cancel);
+
+    always_comb begin
+        next_state = state;
+        case(state)
+            IDLE: begin
+                if (stuff_bit) begin
+                    next_state = CANCEL;
+                end
+            end
+
+            CANCEL: begin
+                if (shift2) begin
+                    next_state = IDLE;
+                end
+            end
+        endcase
+    end
+
+    always_comb begin
+        cancel_next = 1'b0;
+        if (CANCEL) begin
+            cancel_next = 1'b1;
+        end
+    end
 
 endmodule
