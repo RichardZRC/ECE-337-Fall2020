160aebb7383fe37a5ffda0ee24b1905db1b97a73
Richard <zhangruichao2000@gmail.com>
2020-12-01 20:15:06 +0800

    debug

--- Lab12/source/eop_detector.sv
+++ Lab12/source/eop_detector.sv
@@ -1,8 +1,23 @@
 module eop_detector (
     input wire d_plus,
     input wire d_minus,
-    output wire eop
+    output reg eop
 );
-    assign eop = (!d_plus) && (!d_minus);
+
+    reg next_eop;
+    always_comb begin
+        next_eop = 1'b0;
+        if ((!d_plus) && (!d_minus)) begin
+            next_eop = 1'b1;
+        end
+    end
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (n_rst = 1'b0) begin
+            eop <= 1'b0;
+        end else begin
+            eop <= next_eop;
+        end
+    end
 
 endmodule
