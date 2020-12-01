561821b9bc8637e47b96362269f348c1ad88db58
Richard <zhangruichao2000@gmail.com>
2020-12-01 21:26:03 +0800

    debug

--- Lab12/source/sync_high.sv
+++ Lab12/source/sync_high.sv
@@ -8,12 +8,11 @@ module sync_high (
     reg sync_temp;
     always_ff @ (posedge clk, negedge n_rst) begin
         if (1'b0 == n_rst) begin
-            sync_temp <= 1'b1;
+            // sync_temp <= 1'b1;
             sync_out <= 1'b1;
         end
         else begin
-            sync_out <= sync_temp;
-            sync_temp <= async_in;
+            sync_out <= async_in;
         end
     end
 
